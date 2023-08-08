6018da352235f14e2530298e776c7faad793be12
sweet-boyw <1975991462@qq.com>
2023-08-08 22:01:05 +0800

    java字符串中字符出现的次数

---
+++ niuike/src/stringTimes.java
@@ -0,0 +1,22 @@
+/**
+ * 接受一个由字母、数字和空格组成的字符串，和一个字符，然后输出输入字符串中该字符的出现次
+ */
+import java.util.Scanner;
+
+public class stringTimes {
+    public static void main(String[] args) {
+        int times = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入字符串：");
+        String str = scanner.nextLine();
+        System.out.println("请输入字符：");
+        char cha = scanner.next().charAt(0);
+        String[] arr = str.split("");
+        for (int i = 0;i < arr.length; i++){
+            if(arr[i].charAt(0) == cha){
+                times += 1;
+            }
+        }
+        System.out.print(cha + "字符出现了" + times + "次");
+    }
+}
